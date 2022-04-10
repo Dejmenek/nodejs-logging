a74c4e7959178de8c5320f835dfb9fdef08c0d3b
Dejmenek <dejmen14567@gmail.com>
2022-04-10 13:43:43 +0200

    create and login to account

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ const path = require('path');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const { response } = require('express');
+const { application } = require('express');
 
 const app = express();
 const port = process.env.PORT || 5500
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'logging'
+});
+
+connection.connect((error) => {
+    if(error) {
+        console.log("Nie mozna polaczyc sie do bazy");
+        return;
+    } else {
+        console.log("Polaczono sie z baza danych");
+    }
+});
 
 app.use(express.urlencoded({extended: true})); app.use(express.static(path.join(__dirname, '/public')));
 
@@ -17,6 +34,10 @@ app.get('/login', (req,res) => {
     res.sendFile(path.join(__dirname + '/public/views/login.html'))
 })
 
+app.get('/dashboard', (req,res) => {
+    res.sendFile(path.join(__dirname + '/public/views/index.html'))
+});
+
 app.get('/register', (req,res) => {
     res.sendFile(path.join(__dirname + '/public/views/register.html'))
 })
@@ -25,22 +46,39 @@ app.post('/login', (req,res) => {
     let username = req.body.username;
     let password = req.body.password;
 
-    try {
-        if(username && password) {
-            // response.send('Nieprawidłowa nazwa użytkownika lub hasło));
-            // response.end();
-        } else {
-            res.send('Wprowadz nazwe użytkownika i hasło');
+    if(username && password) {
+        connection.query(`SELECT * FROM accounts WHERE username = "${username}" AND password = "${password}"`, (error, results, fields) => {
+            if(error) throw error
+
+            if(results.length > 0) {
+                res.redirect('/dashboard');
+            } else {
+                res.send("Nieprawidłowa nazwa użytkownika lub hasło");
+            }
             res.end();
-        }
-    }
-    catch {
-        res.send('Internal server error');
+
+            
+        })
+    } else {
+        res.send('Wprowadz nazwe użytkownika i hasło');
+        res.end();
     }
 })
 
 app.post('/register', (req,res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    let email = req.body.email;
 
+    if(username && password && email) {
+        connection.query(`INSERT INTO accounts (username, email, password) VALUES ("${username}","${email}","${password}")`, (error,results,fields) => {
+            if(error) throw error;
+            console.log("Dodano użytkownika");
+            res.redirect('/login');
+        });
+    } else {
+        res.send('Wprowadz wszystkie dane');
+    }
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
