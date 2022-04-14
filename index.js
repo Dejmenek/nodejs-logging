c771fe64da0dc46cbc16f96cc65bff614325b6fb
Dejmenek <dejmen14567@gmail.com>
2022-04-14 19:01:40 +0200

    add env variables

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require('express');
+const app = express();
+const flash = require('connect-flash');
+const session = require('express-session');
+
 const path = require('path');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const { response } = require('express');
 
-const app = express();
-const port = process.env.PORT || 5500
+require('dotenv').config()
+
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_DATABASE = process.env.DB_DATABASE
+const DB_PORT = process.env.DB_PORT
+const SESSION_SECRET = process.env.SESSION_SECRET
+
+const port = process.env.PORT
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'logging'
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE
 });
 
 connection.connect((error) => {
@@ -23,45 +35,48 @@ connection.connect((error) => {
     }
 });
 
-app.use(express.urlencoded({extended: true})); app.use(express.static(path.join(__dirname, '/public')));
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine','ejs');
+
+app.use(session({
+    secret: SESSION_SECRET,
+    saveUninitialized: true,
+    resave: true
+}));
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(flash());
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname + '/public/views/home.html'))
+    res.render('home.ejs');
 })
 
 app.get('/login', (req,res) => {
-    res.sendFile(path.join(__dirname + '/public/views/login.html'))
+    res.render('login.ejs');
 })
 
 app.get('/dashboard', (req,res) => {
-    res.sendFile(path.join(__dirname + '/public/views/index.html'))
+    res.render('index.ejs', {msg});
 });
 
 app.get('/register', (req,res) => {
-    res.sendFile(path.join(__dirname + '/public/views/register.html'))
+    res.render('register.ejs');
 })
 
 app.post('/login', (req,res) => {
-    let username = req.body.username;
-    let password = req.body.password;
+    const username = req.body.username;
+    const password = req.body.password;
 
-    if(username && password) {
-        connection.query(`SELECT * FROM accounts WHERE username = "${username}" AND password = "${password}"`, (error, results, fields) => {
-            if(error) throw error
+    connection.query(`SELECT * FROM accounts WHERE username = "${username}" AND password = "${password}"`, (error, results, fields) => {
+        if(error) throw error
 
-            if(results.length > 0) {
-                res.redirect('/dashboard');
-            } else {
-                res.send("Nieprawidłowa nazwa użytkownika lub hasło");
-            }
-            res.end();
-
-            
-        })
-    } else {
-        res.send('Wprowadź nazwę użytkownika i hasło');
-        res.end();
-    }
+        if(results.length > 0) {
+            res.redirect('/dashboard', {msg: `Witaj ${username}`});
+        } else {
+            res.redirect('/login');
+        }
+    })
 })
 
 app.post('/register', (req,res) => {
@@ -69,15 +84,10 @@ app.post('/register', (req,res) => {
     let password = req.body.password;
     let email = req.body.email;
 
-    if(username && password && email) {
-        connection.query(`INSERT INTO accounts (username, email, password) VALUES ("${username}","${email}","${password}")`, (error,results,fields) => {
-            if(error) throw error;
-            console.log("Dodano użytkownika");
-            res.redirect('/login');
-        });
-    } else {
-        res.send('Wprowadź wszystkie dane');
-    }
+    connection.query(`INSERT INTO accounts (username, email, password) VALUES ("${username}","${email}","${password}")`, (error,results,fields) => {
+        if(error) throw error;
+        res.redirect('/login');
+    });
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
